0848c53e14a48b1895f369e817f1769d0d6159d5
Rickson <rickson.simoes@hotmail.com>
2019-12-09 17:08:31 -0300

    styled-components adicionado, configuracao de css global configurada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { Title } from './styles';
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return <Title>Styled components feito</Title>;
 }
