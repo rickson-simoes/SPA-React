46d461d27e2cb4346db790aa247dd930531b1cf5
Rickson <rickson.simoes@hotmail.com>
2019-12-09 12:23:03 -0300

    Roteamento no react configurado, react-router-dom instalado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Inicio</h1>
-    </div>
-  );
+export default function App() {
+  return <Routes />;
 }
-
-export default App;
