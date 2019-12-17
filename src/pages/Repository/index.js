50e1e7e0f5e19d01dae1b0a687313c92e49bfdcd
Rickson <rickson.simoes@hotmail.com>
2019-12-17 18:18:58 -0300

    :hammer: Ao inves de 5 paginas são 8 agora.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -33,7 +33,7 @@ export default class Repository extends Component {
       api.get(`/repos/${repoName}/issues`, {
         params: {
           state: 'open',
-          per_page: 5
+          per_page: 8
         }
       })
     ]);
